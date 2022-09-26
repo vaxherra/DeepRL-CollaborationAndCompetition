c0cfa90d9d8bb24672a391c8f2b4f508844224f9
Robert Kwapich <robert.kwapich@ryvu.com>
2022-09-26 18:15:52 +0200

    Refactor param EPISODES -> scoring_episodes for ease of reading

--- train_utils.py
+++ train_utils.py
@@ -18,8 +18,8 @@ def _get_actions(states, add_noise, agent_0, agent_1):
     return np.concatenate((action_0, action_1), axis=0).flatten()
 
 
-def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, print_every=10, EPISODES=100,
-           ADD_NOISE: bool = True, target_score=0.5):
+def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, print_every=10, scoring_episodes=100,
+           add_noise: bool = True, target_score=0.5):
     """Multi-Agent Deep Deterministic Policy Gradient (MADDPG)
 
     :param env: environment
@@ -30,16 +30,18 @@ def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, 
     :param max_t: maximum number of timesteps per episode
     :param train_mode: boolean to set the environment to train mode
     :param print_every: interval to print the average score
-    :param EPISODES: number of episodes to average the score over
-    :param ADD_NOISE: boolean to add noise to the actions
+    :param scoring_episodes: number of episodes to average the score over
+    :param add_noise: boolean to add noise to the actions
     :param target_score: target score to solve the environment
+
+
+    :return: scores: list of scores from each episode, scores_window: list of scores from the last scoring_episodes
     """
 
     num_agents = 2
-    scores_queue = deque(maxlen=EPISODES)
+    scores_queue = deque(maxlen=scoring_episodes)
     scores_arr = []
     average_score = []
-    best_score = -np.inf
 
     for i_episode in range(1, n_episodes + 1):
         env_info = env.reset(train_mode=train_mode)[brain_name]  # reset environment
@@ -48,7 +50,7 @@ def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, 
         agent_1.reset()
         scores = np.zeros(num_agents)
         while True:
-            actions = _get_actions(states, ADD_NOISE, agent_0, agent_1)  # choose agent actions
+            actions = _get_actions(states, add_noise, agent_0, agent_1)  # choose agent actions
             env_info = env.step(actions)[brain_name]  # send agents' actions to the environment
             next_states = np.reshape(env_info.vector_observations, (1, 48))  # get agents' next states
             rewards = env_info.rewards  # get rewards
@@ -65,11 +67,6 @@ def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, 
         scores_arr.append(episode_best_score)
         average_score.append(np.mean(scores_queue))
 
-        # save best score
-        if episode_best_score > best_score:
-            best_score = episode_best_score
-            best_episode = i_episode
-
         # print results
         if i_episode % print_every == 0:
             print(
@@ -81,7 +78,7 @@ def maddpg(env, brain_name, agent_0, agent_1, n_episodes=2000, train_mode=True, 
         if average_score[-1] >= target_score:
             print('<-- Environment solved in {:d} episodes! \
                 \n<-- Average Score: {:.3f} over past {:d} episodes'.format(
-                i_episode - EPISODES, average_score[-1], EPISODES))
+                i_episode - scoring_episodes, average_score[-1], scoring_episodes))
             # save weights
             torch.save(agent_0.actor_local.state_dict(), 'checkpoint_actor_0.pth')
             torch.save(agent_0.critic_local.state_dict(), 'checkpoint_critic_0.pth')
